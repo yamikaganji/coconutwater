27db3bde99d4fee0f0cabed6512fd0e859392b6b
yamikaganji <yamikaganji@gmail.com>
2021-07-13 20:20:25 +0530

    codechef problem

---
+++ Coldplay.java
@@ -0,0 +1,21 @@
+package codechef;
+
+import java.util.Scanner;
+
+public class Coldplay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int m=sc.nextInt();
+		    int s=sc.nextInt();
+		    int n=m/s;
+		    System.out.println(n);
+		}
+
+	}
+
+}
