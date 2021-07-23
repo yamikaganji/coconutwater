b7f74892cf339649a8fa99372c070200a4f178a4
yamikaganji <yamikaganji@gmail.com>
2021-07-23 21:32:10 +0530

    nth fibonacci number

---
+++ RecFib.java
@@ -0,0 +1,27 @@
+package codechef;
+
+import java.util.Scanner;
+
+public class RecFib {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter n value:");
+		int n=sc.nextInt();
+		int op=fib(n);
+		System.out.print("the fibonacci sequence is:  " +  op   +"   ");
+
+	}
+	public static int fib(int n)
+	{
+		if(n==1)
+			return 0;
+		if(n==2 )
+	
+			return 1;
+	
+		return fib(n-1)+fib(n-2);
+		
+	}
+
+}
